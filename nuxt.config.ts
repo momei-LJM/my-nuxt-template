06d78a04ee4d388c6b710c646b481d0200337e8f
momei <2262754355@qq.com>
2025-06-29 15:17:27 +0800

    build: spa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@element-plus/nuxt', '@nuxt/eslint'],
+  ssr: false,
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css', '~/assets/index.scss'],
   srcDir: 'src',
