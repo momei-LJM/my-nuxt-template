4363666968eb71b08debd32d84ef698b45b2e840
momei <2262754355@qq.com>
2025-06-29 13:57:39 +0800

    style: 格式化

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="text-red-500">
     hellow
-    <el-button type="primary">11</el-button>
+    <el-button type="primary">
+      11
+    </el-button>
   </div>
 </template>
 
 <script lang="ts" setup>
-const a = ref(1);
-a.value = 2;
+const a = ref(1)
+a.value = 2
 </script>
